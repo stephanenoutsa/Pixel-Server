ac3a33bc437b0cd85369545b06cc32a714b1f171
Steph Noutsa <stephanenoutsa@yahoo.com>
2017-05-10 17:57:55 +0100

    Adds logic to scrape jobs from AJ website

--- src/main/java/com/pixel/resource/JobResource.java
+++ src/main/java/com/pixel/resource/JobResource.java
@@ -6,7 +6,6 @@
 package com.pixel.resource;
 
 import com.pixel.database.MyDBHandler;
-import java.io.IOException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -18,6 +17,8 @@ import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.UriInfo;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 /**
  *
@@ -35,16 +36,31 @@ public class JobResource {
     public String getJobs(@Context UriInfo uriInfo) {
         MultivaluedMap<String, String> queryParams = uriInfo.getQueryParameters();
         
-        String query = queryParams.getFirst("query");
+        String search = queryParams.getFirst("search");
+        String category = queryParams.getFirst("category");
+        String location = queryParams.getFirst("location");
         
         String response = "";
         
         try {
-            String url = "http://www.njorku.com/search/jobsearch?query=" + query + "&country=";
-            Document doc = Jsoup.connect(url).get();
-            //Elements newsHeadlines = doc.select("#mp-itn b a");
-            response = doc.text();
-        } catch (IOException e) {
+            String url = "http://www.akwajobs.com/search?search=" + search + "&category=" +
+                    category + "&location=" + location;
+            Document doc = Jsoup.connect(url).timeout(0).get();
+            
+             response += scrapeAkwaJobs(doc);    
+             
+             // Move to next pages            
+            Elements pages = doc.select("ul.pagination > li > a[href]:not(a[rel])");
+            System.out.println(pages.size());
+            
+            for (Element page : pages) {
+                // Load the next page
+                Document next = Jsoup.connect(page.attr("abs:href")).get();
+                System.out.println(page.attr("abs:href"));
+                response += scrapeAkwaJobs(next);
+            }
+            
+        } catch (Exception e) {
             e.printStackTrace();
         }
         
@@ -54,6 +70,30 @@ public class JobResource {
     @POST
     public String addJob(String job) {
         return dbHandler.addJob(job);
+    }
+    
+    // Method to scrape web pages from Akwa Jobs website
+    public String scrapeAkwaJobs(Document doc) {
+        String response = "";
+        
+        Elements container = doc.select("div.col-md-12:gt(2)");
+        
+        for (Element e : container.select("div.content-left > div.media > div.media-body")) {
+            String posted = e.select("p").first().text();
+            String title = e.select("h3.media-heading > a[href]").text();
+            String link = e.select("h3.media-heading > a[href]").attr("href");
+            String town = e.select("h5 > a[href]").first().nextElementSibling().text();
+            String description = e.select("p").text();
+            description = description.substring(0, description.length() - 10) + "...";
+
+            response += "Date posted: " + posted + "\n"
+                    + "Title: " + title + "\n"
+                    + "URL: " + link + "\n"
+                    + "Location: " + town + "\n"
+                    + "Description: " + description + "\n\n";
+        }
+        
+        return response;
     }
     
 }
